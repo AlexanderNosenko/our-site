15f342423b3bd96f3f7e55b5e49fc058da552a26
aseredenko <alexander.seredenko@gmail.com>
2015-06-26 13:13:07 +0300

    Page navigation have been realized

---
+++ js/main.js
@@ -0,0 +1,84 @@
+$(document).ready( function () {
+	
+	// Main navigation
+	$('.page-navigation').find('a').click( function (){
+		var scrollId = $(this).attr('href');
+		$(this).find('a').removeClass('active');
+		$(window).scrollTo(scrollId, 1000, {'margin': 'true'});
+    return false;
+	});	
+
+	// Send a message btn hendler
+	$('.top-line').find('.message-btn').click( function (){
+		$(window).scrollTo('#contact-us', 1000, {'margin': 'true'});	
+	});
+	
+	//Section positions
+	var sectionsPositions = {
+		'about' : 0,
+		'projects' : 883,
+		'reviews' : 1790,
+		'wwd' : 2606,
+		'process' : 3146,
+		'team' : 4299,
+		'contacts' : 4578
+	}
+
+	// Function for getting scrolltop 
+	function getScrollTop(){
+    if(typeof pageYOffset!= 'undefined'){
+      return pageYOffset;
+    }
+    else{
+      var B= document.body; 
+      var D= document.documentElement;
+      D= (D.clientHeight)? D: B;
+      return D.scrollTop;
+    }
+  }
+
+	$(window).scroll( function () {
+		currentScrollPosition = getScrollTop();	
+
+		if (currentScrollPosition >= sectionsPositions.about &&
+				currentScrollPosition < sectionsPositions.projects){
+			hightlightActive('about');
+		}
+		
+		if (currentScrollPosition >= sectionsPositions.projects &&
+				currentScrollPosition < sectionsPositions.reviews){
+			hightlightActive('projects');
+		}
+
+		if (currentScrollPosition >= sectionsPositions.reviews &&
+				currentScrollPosition < sectionsPositions.wwd){
+			hightlightActive('reviews');
+		}
+
+		if (currentScrollPosition >= sectionsPositions.wwd &&
+				currentScrollPosition < sectionsPositions.process){
+			hightlightActive('wwd');
+		}
+
+		if (currentScrollPosition >= sectionsPositions.process &&
+				currentScrollPosition < sectionsPositions.team){
+			hightlightActive('process');
+		}
+
+		if (currentScrollPosition >= sectionsPositions.team &&
+				currentScrollPosition < sectionsPositions.contacts){
+			hightlightActive('team');
+		}
+
+		if (currentScrollPosition >= sectionsPositions.contacts){
+			hightlightActive('contacts');
+		}
+	});
+	
+	function hightlightActive (categoryId){
+
+		categoryId = "." + categoryId;
+		$('.page-navigation').find('a').removeClass('active');
+		$('.page-navigation').find(categoryId).addClass('active');
+	}
+});
